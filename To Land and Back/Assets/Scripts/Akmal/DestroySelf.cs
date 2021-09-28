cdfb0944d2eb9be8dcf08bab1d43469c2d385bca
AkmalZulkifli <akmalzulkifli08@gmail.com>
2021-09-28 16:16:01 +0800

    collision i guess

--- To Land and Back/Assets/Scripts/Akmal/DestroySelf.cs
+++ To Land and Back/Assets/Scripts/Akmal/DestroySelf.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class DestroySelf : MonoBehaviour
 {
     public float time;
+    public GameObject explosion;
+
     void Start()
     {
         Destroy(gameObject, time);
+        Instantiate(explosion, transform.position, Quaternion.identity);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
